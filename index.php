3b424acf8fce0fb65b8d97efe1a5254f3b073a99
Geemley <128781989+Geemley@users.noreply.github.com>
2023-05-31 13:22:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<Doctype html>
+    <html>
+
+
+<head>
+    <title>Учёт технических и материальных активов</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="img/icon.png" type="image/png">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+
+<body>
+    <div class="d_menu">
+    <ul class="menu">
+        <li><a href="index.php">Главная страница</a></li>
+        <li><a href="#">Структура организации</a>
+        <ul class="submenu">
+            <li><a href="departament.php">Информация о структурных подразделениях</a></li>
+        </ul>
+        </li>
+        <li><a href="#">Материальные активы</a>
+        <ul class="submenu">
+            <li><a href="category.php">Категории объектов материальной ответственности</a></li>
+            <li><a href="active.php">Ввод и редактирование информации об активах</a></li>
+            <li><a href="search_active.php">Поиск информации об активах</a></li>
+        </ul>
+        </li>
+    </ul>
+    </div>
+    
+    <img scr="img/banner.png" title="На главную">
+    <br>
+    <div class="d_reg">
+        <a href="reg_from.php">Регистрация</a>/<a href="auth_from.php">Вход</a>
+    </div>
+    <ul>
+        <div class="d_banner" align="center">
+            <a href="index.php"><img scr="img/1.png" title="На главную"></a>
+        </div>
+    </ul>
+    
+    <hr>
+    
+    <table>
+        <tr>
+            <td colspan="2" align="center">
+                <font size="5" color="MediumSlateBlue"><b>Задачи приложения</b></font>
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <img src="img/ris1.png" width="100" height="99">
+            </td>
+            <td align="left">
+                <b>Учет и контроль наличия и движения материальных и технических активов</b> внутри структурного подразделения без использования специализированных программ бухгалтерского учета.
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <img src="img/ris2.png" width="100" height="100">
+            </td>
+            <td align="left">
+                <u>Удобный формат доступа к данным</u> посредством форм и запросов.
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <img src="img/ris3.png" width="102" height="100">
+            </td>
+            <td align="left">
+                <i>Фиксация всех операций</i>, производимых над объектами материального учета.
+            </td>
+        </tr>
+    </table>
+    
+    <hr>
+</body>
+
+
+
+
+</body>
+</html>
